d08a9eb9cfeaf6318e1586abb4a59a40e2462335
superfreshbrain <pray4higashinihon@gmail.com>
2018-09-02 11:17:41 +0900

    migration file AddImageToPosts migrated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,11 +24,11 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(stuff_name: task_params[:stuff_name], description: task_params[:description], comment: task_params[:comment], user_id: current_user.id)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: '投稿を作成しました。' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, notice: '投稿の編集が完了しました。' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: '投稿を削除しました。' }
       format.json { head :no_content }
     end
   end
