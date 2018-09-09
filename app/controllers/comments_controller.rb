7add301ca20a732ceb224eab607591918f55566a
superfreshbrain <pray4higashinihon@gmail.com>
2018-09-09 21:14:52 +0900

    comment function working nowgit status

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(text: comment_params[:text], post_id: comment_params[:post_id], user_id: current_user.id)
+    redirect_to "/posts/#{@comment.post.id}" # back to the same post after submitting comment
+  end
+
+  private
+
+  def comment_params
+    params.permit(:text, :post_id)
+  end
 end
