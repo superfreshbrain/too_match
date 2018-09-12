1b1680f64499efd623fe18786539538cb4cb3cb9
superfreshbrain <pray4higashinihon@gmail.com>
2018-09-12 19:25:42 +0900

    defined create and destroy action in likes_controller. ready to implement view(partial template)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, post_id: like_params[:post_id])
+    @likes = Like.where(post_id: like_params[:post_id]) # for total likes
+    @posts = Post.all
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, post_id: like_params[:post_id])
+    like.destroy
+
+    @likes = Like.where(post_id: like_params[:post_id])  # for total likes
+    @posts = Post.all
+  end
+
+  private
+
+  def like_params
+    params.permit(:post_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # resources method creates 7 default actions(index, show, new, create, edit, update, destroy)
   resources :posts do
-    resources :comments, only: [:create] # nested to use posts id info in comments
-    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create] # nested to use posts id info(post_id) in comments
+    resources :likes, only: [:create, :destroy] # nested to use posts id info(post_id) in likes
   end
 
   resources :users, only: [:show] # creates show action only
